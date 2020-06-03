44567493b5233b88847b14dff1d2661e8d551ea2
Tristan Depew <tristan.depew@gmail.com>
2020-06-03 18:28:03 -0400

    finished word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,32 @@
+import re
+
+
 def word_count(s):
-    # Your code here
+    counts = {}
+
+    stripped = re.sub(r'[^\w\d\s\']+', '', s)
+    for c in stripped.split():
+        c = c.lower()
+        if c in counts:
+            counts[c] += 1
+
+        else:
+            counts[c] = 1
+
+    return counts
 
 
+# Regex explaienr:
+# [^] matches everything but what is inside block quotes
+# \w matches any word character
+# \d matches any digit
+# \s matches any whitespace
+# \' matches the character '
+# +  matches between one and unlimited times, as many times as possible, giving back as needed
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
